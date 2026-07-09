6f946d085e5581ba904bf5ad585ea05a226b4521
Gagana BN <bngagana401@gmail.com>
2026-07-09 23:04:44 +0530

    update

--- src/main/java/com/sak/entity/Product.java
+++ src/main/java/com/sak/entity/Product.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Lob;
-import lombok.Data;
+import lombok.Data
 
 @Entity
 @Data
